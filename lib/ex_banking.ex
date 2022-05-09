83e6a42ae2d80fd700db1d414604f816aa836ed9
ChaubeyAshutosh <ashutoshchaubey9318@gmail.com>
2022-05-09 13:49:15 +0530

    ex_banking implementation

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -1,18 +1,108 @@
 defmodule ExBanking do
   @moduledoc """
-  Documentation for `ExBanking`.
+  Main Module to interact with the banking application
   """
+
+  alias ExBanking.User
+  alias ExBanking.UserSupervisor
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExBanking.hello()
-      :world
-
+  This function will create a user process to save currency and amount
+  Example
+  iex> ExBanking.create_user("user")
+  :ok
   """
-  def hello do
-    :world
+  @spec create_user(user :: String.t()) :: :ok | {:error, :wrong_arguments | :user_already_exists}
+  def create_user(user) when is_binary(user) do
+    case UserSupervisor.new_user(user) do
+      {:ok, _pid} -> :ok
+      error -> error
+    end
   end
+
+  def create_user(_user), do: {:error, :wrong_arguments}
+
+  @doc """
+  This function will deposit money to a user process
+  Example
+  iex> ExBanking.deposit("user", 5, "usd")
+  {:ok, 5.0}
+  """
+  @spec deposit(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  def deposit(user, amount, currency)
+      when is_binary(user) and is_integer(amount) and amount >= 0 and is_binary(currency) do
+    User.request(user, {:deposit, amount, currency})
+  end
+
+  def deposit(_user, _amount, _currency), do: {:error, :wrong_arguments}
+
+  @doc """
+  This function will withdraw money from a user process
+  Example
+  iex> ExBanking.withdraw("user", 5, "usd")
+  {:ok, 0.0}
+  """
+  @spec withdraw(user :: String.t(), amount :: number, currency :: String.t()) ::
+          {:ok, new_balance :: number}
+          | {:error,
+             :wrong_arguments
+             | :user_does_not_exist
+             | :not_enough_money
+             | :too_many_requests_to_user}
+  def withdraw(user, amount, currency)
+      when is_binary(user) and is_integer(amount) and amount >= 0 and is_binary(currency) do
+    User.request(user, {:withdraw, amount, currency})
+  end
+
+  def withdraw(_user, _amount, _currency), do: {:error, :wrong_arguments}
+
+  @doc """
+  This function will return current balance from a user process for a particular currency
+  Example
+  iex> ExBanking.get_balance("user", "usd")
+  {:ok, 5.0}
+  """
+  @spec get_balance(user :: String.t(), currency :: String.t()) ::
+          {:ok, balance :: number}
+          | {:error, :wrong_arguments | :user_does_not_exist | :too_many_requests_to_user}
+  def get_balance(user, currency) when is_binary(user) and is_binary(currency) do
+    User.request(user, {:get_balance, currency})
+  end
+
+  def get_balance(_, _), do: {:error, :wrong_arguments}
+
+  @doc """
+  This function will send money from a user process to a another user process
+  Example
+  iex> ExBanking.send("sender", "receiver", 5, "usd")
+  {:ok, 5.0, 5.0}
+  """
+  @spec send(
+          from_user :: String.t(),
+          to_user :: String.t(),
+          amount :: number,
+          currency :: String.t()
+        ) ::
+          {:ok, from_user_balance :: number, to_user_balance :: number}
+          | {:error,
+             :wrong_arguments
+             | :not_enough_money
+             | :sender_does_not_exist
+             | :receiver_does_not_exist
+             | :too_many_requests_to_sender
+             | :too_many_requests_to_receiver}
+  def send(from_user, to_user, amount, currency)
+      when is_binary(from_user) and is_binary(to_user) and is_integer(amount) and amount >= 0 and
+             is_binary(currency) do
+    opts = [
+      user_exist_error_msg: :sender_does_not_exist,
+      requests_limit_error_msg: :too_many_requests_to_sender
+    ]
+
+    User.request(from_user, {:send, to_user, amount, currency}, opts)
+  end
+
+  def send(_from_user, _to_user, _amount, _currency), do: {:error, :wrong_arguments}
 end
--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -1,8 +1,164 @@
 defmodule ExBankingTest do
   use ExUnit.Case
-  doctest ExBanking
 
-  test "greets the world" do
-    assert ExBanking.hello() == :world
+  setup_all do
+    ExBanking.create_user("user1")
+    ExBanking.create_user("user2")
+    :ok
+  end
+
+  describe "&create_user/1" do
+    test "create new user when args is string" do
+      ExBanking.create_user("newuser")
+      refute Registry.lookup(Registry.Users, "newuser") == []
+    end
+
+    test "returns error response when user already exists" do
+      assert ExBanking.create_user("user1") == {:error, :user_already_exists}
+    end
+
+    test "returns error response when args is not string" do
+      assert ExBanking.create_user(:hello) == {:error, :wrong_arguments}
+    end
+  end
+
+  describe "&deposit/1" do
+    test "deposit money correctly" do
+      assert {:ok, _balance} = ExBanking.deposit("user1", 5, "usd")
+    end
+
+    test "returns error response when user does not exist" do
+      assert ExBanking.deposit("random", 5, "usd") == {:error, :user_does_not_exist}
+    end
+
+    test "returns error response when we give bad args" do
+      assert ExBanking.deposit("user1", "5", "usd") == {:error, :wrong_arguments}
+    end
+
+    test "returns error response when there is too many requests" do
+      ExBanking.create_user("deposit")
+
+      error_count =
+        1..100
+        |> Enum.map(fn _ ->
+          Task.async(fn -> ExBanking.deposit("deposit", 5, "usd") end)
+        end)
+        |> Enum.map(&Task.await/1)
+        |> Enum.count(fn result -> result !== {:error, :too_many_requests_to_user} end)
+
+      assert error_count >= 1
+    end
+  end
+
+  describe "&withdraw/1" do
+    test "withdraw money correctly" do
+      ExBanking.deposit("user2", 10, "usd")
+      assert {:ok, _balance} = ExBanking.withdraw("user2", 5, "usd")
+    end
+
+    test "withdraw more money than the user actually have" do
+      assert ExBanking.withdraw("user1", 500, "usd") == {:error, :not_enough_money}
+    end
+
+    test "returns error response when user does not exist" do
+      assert ExBanking.withdraw("random", 5, "usd") == {:error, :user_does_not_exist}
+    end
+
+    test "returns error response when we give bad args" do
+      assert ExBanking.withdraw("user1", "5", "usd") == {:error, :wrong_arguments}
+    end
+
+    test "returns error response when there is too many requests" do
+      ExBanking.create_user("withdraw")
+
+      error_count =
+        1..100
+        |> Enum.map(fn _ ->
+          Task.async(fn -> ExBanking.withdraw("withdraw", 5, "usd") end)
+        end)
+        |> Enum.map(&Task.await/1)
+        |> Enum.count(fn result -> result !== {:error, :too_many_requests_to_user} end)
+
+      assert error_count >= 1
+    end
+  end
+
+  describe "&get_balance/1" do
+    test "returns balance correctly" do
+      assert {:ok, _balance} = ExBanking.get_balance("user1", "usd")
+    end
+
+    test "returns error response when user does not exist" do
+      assert ExBanking.get_balance("random", "usd") == {:error, :user_does_not_exist}
+    end
+
+    test "returns error response when we give bad args" do
+      assert ExBanking.get_balance("user1", :usd) == {:error, :wrong_arguments}
+    end
+
+    test "returns error response when there is too many requests" do
+      ExBanking.create_user("balance")
+
+      error_count =
+        1..100
+        |> Enum.map(fn _ ->
+          Task.async(fn -> ExBanking.get_balance("balance", "usd") end)
+        end)
+        |> Enum.map(&Task.await/1)
+        |> Enum.count(fn result -> result !== {:error, :too_many_requests_to_user} end)
+
+      assert error_count >= 1
+    end
+  end
+
+  describe "&send/1" do
+    test "send money correctly" do
+      ExBanking.deposit("user1", 5, "usd")
+      assert {:ok, _from, _to} = ExBanking.send("user1", "user2", 5, "usd")
+    end
+
+    test "returns error response when from user does not exist" do
+      assert ExBanking.send("random", "user2", 5, "usd") == {:error, :sender_does_not_exist}
+    end
+
+    test "returns error response when to user does not exist" do
+      ExBanking.deposit("user1", 5, "usd")
+      assert ExBanking.send("user1", "to", 5, "usd") == {:error, :receiver_does_not_exist}
+    end
+
+    test "returns error response when we give bad args" do
+      assert ExBanking.send("user1", "user2", "5", :usd) == {:error, :wrong_arguments}
+    end
+
+    test "returns error response when there is too many requests for sender" do
+      ExBanking.create_user("sender")
+      ExBanking.create_user("receiver")
+
+      error_count =
+        1..100
+        |> Enum.map(fn _ ->
+          Task.async(fn -> ExBanking.send("sender", "receiver", 5, "usd") end)
+        end)
+        |> Enum.map(&Task.await/1)
+        |> Enum.count(fn result -> result !== {:error, :too_many_requests_to_sender} end)
+
+      assert error_count >= 1
+    end
+
+    test "returns error response when there is too many requests for receiver" do
+      ExBanking.create_user("sender")
+      ExBanking.create_user("receiver")
+      ExBanking.deposit("sender", 1000, "usd")
+
+      error_count =
+        1..100
+        |> Enum.map(fn _ ->
+          Task.async(fn -> ExBanking.send("sender", "receiver", 5, "usd") end)
+        end)
+        |> Enum.map(&Task.await/1)
+        |> Enum.count(fn result -> result !== {:error, :too_many_requests_to_receiver} end)
+
+      assert error_count >= 1
+    end
   end
 end
